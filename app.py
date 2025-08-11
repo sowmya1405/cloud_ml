0e3501f3cbdf27e9bbcfab881827a4d28c3e06be
sowmya jujjuri <sowmyajujjuri93@gmail.com>
2025-08-11 14:41:51 +0530

    Fix model folder and add iris_model.joblib

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, request, jsonify
+import numpy as np
+import pickle
 
 app = Flask(__name__)
 
+# Load model
+model = pickle.load(open('model.pkl', 'rb'))
+
 @app.route('/')
-def home():
-    return '🌸 Iris Classifier API is running!'
+def index():
+    return "🌸 Iris Classifier API is running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    prediction = model.predict(np.array(data['data']))
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
